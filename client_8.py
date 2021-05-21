c997f3faeb4d4bf973a068ace734100f31287add
aribianchi <55762431+aribianchi@users.noreply.github.com>
2021-05-21 13:17:06 +0200

    client 1,5,8

---
+++ client_8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import socket
+from decimal import Decimal
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('10.40.0.46', 9997)
+s.connect(server_address)
+
+get = "GET / HTTP/1.0\r\n" + "Host:10.40.0.46:9997\r\n\n"
+
+s.send(get.encode())
+request = s.recv(1024)
+request = request.decode()
+print(get)
+print(request)
+
+
+s.close()
