f7a25d8e90de85f515c1278b5478dd7891c236b8
Johan Jacob <johan.jacob@usi.ch>
2021-05-23 12:42:47 +0200

    working version

--- client_3.py
+++ client_3.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 
 import socket
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+sock1.connect(('10.40.0.46', 9992))
+data = sock1.recv(128)
+sock1.close()
+new_port = int(data.decode('ascii').replace('random port: ', ''))
+print("random port: " + str(new_port))
+sock2.connect(('10.40.0.46', new_port))
+data = sock2.recv(128)
+print(data.decode('utf-8'))
+sock2.close()
 
-DEST_IP = '10.40.0.46'
-DEST_PORT = 9992
-
-
-def main():
-    sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-
-    sock1.connect((DEST_IP, DEST_PORT))
-    data = sock1.recv(128)
-    sock1.close()
-
-    new_port = int(data.decode('ascii').replace('random port: ', ''))
-
-    sock2.connect((DEST_IP, new_port))
-    data = sock2.recv(128)
-    print(data.decode('utf-8'))
-    sock2.close()
-
-
-if __name__ == "__main__":
-    main()
